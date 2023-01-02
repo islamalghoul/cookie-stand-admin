4192b170f46ce075d7ca577d100dd3f2f8438ad0
islam alghoul <islamalghoul1997>
2023-01-02 12:35:30 +0200

    lab32

---
+++ app/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function Footer() {
+  return (
+    <div className='flex bg-emerald-500 pl-1 fixed left-0 bottom-0 w-full'>
+    <img src='/copyrightlogo.png' alt="img" className=' w-2 h-2 mt-2 bg-emerald-500' />
+    <p >2021 </p>
+    </div>  )  
+}
---
+++ app/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+   <h1 className='bg-emerald-500 pl-1 text-3xl font-bold'>Cookie Stand Admin</h1>
+  )
+}
