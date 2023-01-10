6f52ea7f0c8f8ec94ce3052ef6ced014797cb189
islam <islamalghoul1997@gmail.com>
2023-01-10 03:23:03 -0800

    lab-35

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,17 @@
 "use client"
-import { Inter } from '@next/font/google'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Main from './components/Main'
-import  {useContext, useState} from 'react';
+import React , {useContext, useState} from 'react';
 import LoginForm from './components/LoginForm'
-import ThemeWrapper from './components/context/theme'
-import {AuthWrapper} from './components/context/auth'
+
 import { AuthContext } from './components/context/auth'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
-  const {tokens} = useContext(AuthContext)
   const[flage,setFlage]=useState(false)
+  const {tokens} = useContext(AuthContext);
   let handelClick=(flage)=>{
     setFlage(flage)
     
@@ -25,16 +23,13 @@ export default function Home() {
   return (<>
     
     <div className='bg-indigo-100m dark:bg-black'>
-    < ThemeWrapper>
 
-    <AuthWrapper>
       {tokens ?<><Header handelClick={handelClick}/>
     
     <Main flage={flage}/>
     <Footer flage={flage}/></>:<LoginForm/>}
    
-    </AuthWrapper>
-    </ThemeWrapper>
+    
     </div>
   
     </>
